db0df122c10fe42c6bf24b738319f7e1cd208107
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-03-01 10:07:21 -0600

    psql set up for local development

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -27,8 +27,8 @@ if (process.env.DATABASE_URL) {
 } else {
   config = {
     host: "localhost", // Server hosting the postgres database
-    port: 5432, // env var: PGPORT
-    database: "prime_app", // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+    port: process.env.PG_PORT, // env var: PGPORT
+    database: process.env.PG_DATABASE, // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
