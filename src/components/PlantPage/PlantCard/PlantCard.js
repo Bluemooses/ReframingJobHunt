1bce7634bc04da86221cbe334f83251bef1ae5e9
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-03-01 12:08:18 -0600

    images now display in a list

--- src/components/PlantPage/PlantCard/PlantCard.js
+++ src/components/PlantPage/PlantCard/PlantCard.js
@@ -2,9 +2,13 @@ import React, { useState } from "react";
 
 const PlantCard = ({ plant }) => {
   console.log(plant);
+
   return (
     <div>
       <p>{plant.common_name}</p>
+      <img src={plant.image_url} alt="Unavailable" />
+      <p>Family: {plant.family}</p>
+      <p>Genus: {plant.genus}</p>
     </div>
   );
 };
