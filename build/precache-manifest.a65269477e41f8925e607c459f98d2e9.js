d8d242c47aec118f888a5cd5c29b19717160b1a3
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-03-01 08:01:33 -0600

    Added config for netlify

---
+++ build/precache-manifest.a65269477e41f8925e607c459f98d2e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460a9f365360c1b69d9788cf18aa53c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69c06a74e66dff86512a",
+    "url": "/static/css/main.748ce5a3.chunk.css"
+  },
+  {
+    "revision": "567de6f9da620413c752",
+    "url": "/static/js/2.73c5fe97.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.73c5fe97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c06a74e66dff86512a",
+    "url": "/static/js/main.700f744e.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  }
+]);
