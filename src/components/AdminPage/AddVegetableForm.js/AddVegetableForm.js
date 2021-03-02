f183cef500761d0dcb251831b914d07863ec70de
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-03-02 11:08:00 -0600

    Added framework for AdminPage and AddVegetableForm

---
+++ src/components/AdminPage/AddVegetableForm.js/AddVegetableForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddVegetableForm = () => {
+  <div></div>;
+};
+
+export default AddVegetableForm;
---
+++ src/components/AdminPage/AdminPage.js
@@ -0,0 +1,5 @@
+const AdminPage = () => {
+  <div></div>;
+};
+e;
+export default AdminPage;
