d2f4aa28555281a997dae353819907d3348ec6c7
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-03-02 10:36:52 -0600

    Added the vegetable table to heroku db

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -26,9 +26,9 @@ const Nav = (props) => (
       <Link className="nav-link" to="/about">
         About
       </Link>
-      <Link className="nav-link" to="/plants">
+      {/* <Link className="nav-link" to="/plants">
         Plants
-      </Link>
+      </Link> */}
     </div>
   </div>
 );
