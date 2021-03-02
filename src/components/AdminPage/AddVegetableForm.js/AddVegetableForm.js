b27af691bbd1c97724f88a9c725ea4807690bc38
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-03-02 12:06:41 -0600

    Added styling & comments

--- src/components/AdminPage/AddVegetableForm.js/AddVegetableForm.js
+++ src/components/AdminPage/AddVegetableForm.js/AddVegetableForm.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { Stack, Input, Button, Label } from "@chakra-ui/react";
+//Style imports
+import {
+  Stack,
+  Input,
+  Button,
+  Label,
+  Flex,
+  FormControl,
+} from "@chakra-ui/react";
+import "./AddVegetableForm.scss";
 
 const AddVegetableForm = () => {
   // This object matches a database entry in the vegetable table [search_count, id, and url] omitted
@@ -12,18 +21,45 @@ const AddVegetableForm = () => {
     yield_per_sq_ft: "",
     search_count: "",
   };
+
+  //Handles the call to our API to create a new vegetable entry in db
   const AddPlant = () => {};
 
   return (
-    <Stack spacing={4}>
-      <Input placeholder="Name" type="text" />
-      <Input placeholder="Description" type="text" />
-      <Input placeholder="Seed spacing in square inches" type="number" />
-      <Input placeholder="Date to plant" type="date" />
-      <Input placeholder="Days until harvest" type="number" />
-      <Input placeholder="Yield per square ft." type="number" />
-      <Button onClick={AddPlant()}></Button>
-    </Stack>
+    <div className="AddVegetableFormWrapper">
+      <Stack spacing={2}>
+        <Input focusBorderColor="green.600" placeholder="Name" type="text" />
+        <Input
+          textAlign="center"
+          focusBorderColor="green.600"
+          placeholder="Description"
+          type="text"
+        />
+        <Input
+          focusBorderColor="green.600"
+          placeholder="Seed spacing in square inches"
+          type="number"
+        />
+        <Input
+          focusBorderColor="green.600"
+          placeholder="Date to plant"
+          type="date"
+        />
+        <Input
+          focusBorderColor="green.600"
+          placeholder="Days until harvest"
+          type="number"
+        />
+        <Input
+          focusBorderColor="green.600"
+          placeholder="Yield per square ft."
+          type="number"
+        />
+        <Button type="submit" onClick={AddPlant()}>
+          Add Plant
+        </Button>
+      </Stack>
+    </div>
   );
 };
 
