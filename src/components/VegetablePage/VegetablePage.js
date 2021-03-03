62142fb3bb2e6e6f1b4951298de4879b8c0369b2
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-03-03 09:39:59 -0600

    Making the card map stackable adds responsiveness for mobile

--- src/components/VegetablePage/VegetablePage.js
+++ src/components/VegetablePage/VegetablePage.js
@@ -6,7 +6,7 @@ const VegetablePage = () => {
   // We can just use the useSelector hook to select only vegetables from our redux store.
   const vegetables = useSelector((state) => state.vegetables);
   return (
-    <Card.Group>
+    <Card.Group stackable itemsPerRow={4}>
       {vegetables.map((vegetable) => {
         return <VegetableCard key={vegetable.id} vegetable={vegetable} />;
       })}
