7ad17f862dc2adf513dd3e56ff3ca72cb362ccbf
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-03-03 00:23:33 -0600

    last commit for tonight

--- src/components/VegetablePage/VegetableCard/VegetableCard.js
+++ src/components/VegetablePage/VegetableCard/VegetableCard.js
@@ -19,20 +19,22 @@ const VegetableCard = ({ vegetable }) => {
   const { description } = vegetable;
   return (
     <GridItem>
-      <Image objectFit="cover" src={url}></Image>
-      <Accordion allowToggle>
-        <AccordionItem>
-          <AccordionButton>
-            <Box flex="1">Details</Box>
-            <AccordionIcon />
-            <AccordionPanel pb="4">{description}</AccordionPanel>
-          </AccordionButton>
-        </AccordionItem>
-      </Accordion>
-      {/* <Box color="whitesmoke" bg="green.700" p="6">
+      <Box>
+        <Image objectFit="cover" src={url}></Image>
+        <Accordion allowToggle>
+          <AccordionItem>
+            <AccordionButton>
+              <Box flex="1">Details</Box>
+              <AccordionIcon />
+              <AccordionPanel pb="4">{description}</AccordionPanel>
+            </AccordionButton>
+          </AccordionItem>
+        </Accordion>
+        {/* <Box color="whitesmoke" bg="green.700" p="6">
         <Box fontWeight="bold">{name}</Box>
         <Box size="sm">{description}</Box>
       </Box> */}
+      </Box>
     </GridItem>
   );
 };
--- src/components/VegetablePage/VegetablePage.js
+++ src/components/VegetablePage/VegetablePage.js
@@ -6,7 +6,13 @@ const VegetablePage = () => {
   // We can just use the useSelector hook to select only vegetables from our redux store.
   const vegetables = useSelector((state) => state.vegetables);
   return (
-    <Grid templateColumns="repeat(4, 1fr)">
+    <Grid
+      autoFlow
+      autoRows
+      autoColumns
+      templateColumns="repeat(5, 1fr)"
+      gap={6}
+    >
       {vegetables.map((vegetable) => {
         return <VegetableCard key={vegetable.id} vegetable={vegetable} />;
       })}
