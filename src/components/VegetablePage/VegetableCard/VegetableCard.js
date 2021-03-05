28c9633be03afec66784035214a08648c561b0dc
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-03-05 11:54:17 -0600

    Added moment for date/time manipulation

--- src/components/VegetablePage/VegetableCard/VegetableCard.js
+++ src/components/VegetablePage/VegetableCard/VegetableCard.js
@@ -10,23 +10,38 @@ import {
   AccordionPanel,
   AccordionButton,
 } from "@chakra-ui/react";
-import { Card, Image } from "semantic-ui-react";
+import { Card, Image, List, ListItem } from "semantic-ui-react";
 
 const VegetableCard = ({ vegetable }) => {
   // Here's what vegetable gives us access to
   const { url } = vegetable;
   const { name } = vegetable;
   const { description } = vegetable;
+  const { days_to_harvest } = vegetable;
+  const { seed_spacing_area_sq_in } = vegetable;
+  const { yield_per_sq_ft } = vegetable;
+  const { date_to_plant } = vegetable;
+  const date_to_display = new Date(date_to_plant);
+
   return (
     <Card>
       <Card.Content>
+        <Heading>{name}</Heading>
         <Image src={url} />
         <Accordion allowToggle>
           <AccordionItem>
             <AccordionButton>
-              <Box flex="1">Details</Box>
+              <Box flex="1">Vegetable Details</Box>
               <AccordionIcon />
-              <AccordionPanel pb="4">{description}</AccordionPanel>
+              <AccordionPanel pb="4">
+                Seed Spacing Area: {seed_spacing_area_sq_in} sq. in.
+              </AccordionPanel>
+              <AccordionPanel pb="4">
+                Days to Harvest: {days_to_harvest}
+              </AccordionPanel>
+              <AccordionPanel pb="4">
+                When to Plant: {date_to_plant}
+              </AccordionPanel>
             </AccordionButton>
           </AccordionItem>
         </Accordion>
