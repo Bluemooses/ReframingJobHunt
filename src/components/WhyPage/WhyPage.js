447dde4226952304866f014b8ad9bf5d32d90100
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-03-10 19:52:53 -0600

    Removed unec. Containers <>

--- src/components/WhyPage/WhyPage.js
+++ src/components/WhyPage/WhyPage.js
@@ -3,30 +3,24 @@ import React from "react";
 import { Container, Divider, List, ListItem } from "semantic-ui-react";
 
 const WhyPage = () => (
-  <Container>
-    <Container textAlign="center">
-      <Heading>Plant Minnesota</Heading>
-    </Container>
-    <Container textAlign="center">
-      <Text fontFamily="Bookman">
-        Plant Minnesota was created to bring awareness to the untapped resources
-        of urban/suburban land which can be utilized to promote self reliance.
-      </Text>
-    </Container>
+  <Container textAlign="center">
+    <Heading>Plant Minnesota</Heading>
+    <Text fontFamily="Bookman">
+      Plant Minnesota was created to bring awareness to the untapped resources
+      of urban/suburban land which can be utilized to promote self reliance.
+    </Text>
     <Divider />
-    <Container textAlign="center">
-      <Heading>What We Do</Heading>
-      <Text fontFamily="Bookman">
-        <List>
-          <ListItem>
-            Compile data on ideal planting times, theoretical yields of planted
-            crops, days to harvest, and useful tidbits to get the most out of
-            your plants.
-          </ListItem>
-          <ListItem>Use our data to plan garden beds.</ListItem>
-        </List>
-      </Text>
-    </Container>
+    <Heading>What We Do</Heading>
+    <Text fontFamily="Bookman">
+      <List>
+        <ListItem>
+          Compile data on ideal planting times, theoretical yields of planted
+          crops, days to harvest, and useful tidbits to get the most out of your
+          plants.
+        </ListItem>
+        <ListItem>Use our data to plan garden beds.</ListItem>
+      </List>
+    </Text>
   </Container>
 );
 
