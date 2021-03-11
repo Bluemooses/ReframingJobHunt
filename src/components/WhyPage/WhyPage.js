d4957f11aaf61fc671b31708d53c0f065e9a62e1
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-03-10 19:50:45 -0600

    Added basic outline for about page

--- src/components/WhyPage/WhyPage.js
+++ src/components/WhyPage/WhyPage.js
@@ -1,17 +1,30 @@
 import { Heading, Text } from "@chakra-ui/layout";
 import React from "react";
-import { Container, Divider } from "semantic-ui-react";
+import { Container, Divider, List, ListItem } from "semantic-ui-react";
 
 const WhyPage = () => (
   <Container>
     <Container textAlign="center">
-      <Heading>Plant Minnesota: Why?</Heading>
+      <Heading>Plant Minnesota</Heading>
     </Container>
-    <Container textAlign="left">
+    <Container textAlign="center">
       <Text fontFamily="Bookman">
         Plant Minnesota was created to bring awareness to the untapped resources
-        of urban/suburban land which can be utilized to promote self reliance,
-        and reduce reliance on corporate farms.
+        of urban/suburban land which can be utilized to promote self reliance.
+      </Text>
+    </Container>
+    <Divider />
+    <Container textAlign="center">
+      <Heading>What We Do</Heading>
+      <Text fontFamily="Bookman">
+        <List>
+          <ListItem>
+            Compile data on ideal planting times, theoretical yields of planted
+            crops, days to harvest, and useful tidbits to get the most out of
+            your plants.
+          </ListItem>
+          <ListItem>Use our data to plan garden beds.</ListItem>
+        </List>
       </Text>
     </Container>
   </Container>
