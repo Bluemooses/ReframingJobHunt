b09c92c6fb3c3cd6339fe60a19dc9e1cca2f2646
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-03-16 19:11:03 -0500

    Adjusted layout and visual appeal of nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,39 +5,48 @@ import LogOutButton from "../LogOutButton/LogOutButton";
 import "./Nav.scss";
 
 const Nav = (props) => (
-  <div className="nav">
-    <div className="nav-right">
-      <Link className="nav-link" to="/home">
-        {/* Show this link if they are logged in or not,
+  <nav>
+    <ul>
+      <li>
+        <Link className="nav-link" to="/home">
+          {/* Show this link if they are logged in or not,
         but call this link 'Home' if they are logged in,
         and call this link 'Login / Register' if they are not */}
-        {props.user.id ? "Home" : "Login / Register"}
-      </Link>
-      {/* Show the link to the info page and the logout button if the user is logged in */}
-      {props.user.id && (
-        <>
-          <Link className="nav-link" to="/info">
-            Info Page
-          </Link>
-          <LogOutButton className="nav-link" />
-        </>
-      )}
+          {props.user.id ? "Home" : "Login / Register"}
+        </Link>
+      </li>
+
       {/* Show this Link if a user is an Admin */}
       {props.user.admin && (
-        <Link className="nav-link" to="/admin">
-          Admin
-        </Link>
+        <li>
+          <Link className="nav-link" to="/admin">
+            Admin
+          </Link>
+        </li>
       )}
-      {/* Always show this link since the about page is not protected */}
-      <Link className="nav-link" to="/about">
-        Why
-      </Link>
 
-      <Link className="nav-link" to="/vegetables">
-        Vegetables
-      </Link>
-    </div>
-  </div>
+      {/* Always show this link since the about page is not protected */}
+      <li>
+        <Link className="nav-link" to="/about">
+          Information
+        </Link>
+      </li>
+
+      <li>
+        <Link className="nav-link" to="/vegetables">
+          Vegetables
+        </Link>
+      </li>
+      {/* Show the link to the info page and the logout button if the user is logged in */}
+      {props.user.id && (
+        <li>
+          <a href="#" onClick={() => props.dispatch({ type: "LOGOUT" })}>
+            Logout
+          </a>
+        </li>
+      )}
+    </ul>
+  </nav>
 );
 
 // Instead of taking everything from state, we just want the user
